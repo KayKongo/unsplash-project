2dbe86937e979d25ce241fcd3d2a5b4d998840ae
Prince Kongo <princekongo@Princes-MacBook-Pro.local>
2026-07-31 13:19:51 +0000

    feat: added dark mode

--- src/context.jsx
+++ src/context.jsx
@@ -3,10 +3,20 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const greeting = "hello";
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    const newDarkThem = !isDarkTheme;
+    setIsDarkTheme(newDarkThem);
+
+    const bodySelector = document.querySelector("body");
+    bodySelector.classList.toggle("dark-theme", newDarkThem);
+  };
 
   return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
