d31e335d26f28ea77d2e23e2dd35fbcf9ac64650
Prince Kongo <princekongo@Princes-MacBook-Pro.local>
2026-08-02 04:47:02 +0000

    feat: added the search field

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,5 +1,25 @@
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.elements);
+  };
+
+  return (
+    <section>
+      <h1 className="title">free images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cars"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
